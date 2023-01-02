337f11ac7a50529224b3fcc955a93d536608e94b
damiankliman <klimandamian@gmail.com>
2023-01-02 01:43:41 -0800

    allow custom max numbers

--- core/responses.py
+++ core/responses.py
@@ -6,8 +6,18 @@ def handle_response(message):
   if p_message == 'hello':
     return "Hi there!"
 
-  if p_message == 'roll':
-    return str(random.randint(1, 99))
+  if p_message.startswith('roll'):
+    max = 99
+
+    if len(message.split()) > 1:
+      custom_max = message.split()[1]
+
+      if custom_max.isdigit():
+        max = int(custom_max)
+      else:
+        return "Please enter a valid number"
+
+    return str(random.randint(1, max))
 
   if p_message == 'help':
     return "`This is a help message`"
