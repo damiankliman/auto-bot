d40bf523a2401a3460a7e88c1ff5a6f2ffd8e838
damiankliman <klimandamian@gmail.com>
2023-01-02 23:37:10 -0800

    fix indentation

--- core/models.py
+++ core/models.py
@@ -6,8 +6,8 @@ from core.database import Base
 import uuid
 
 class User(Base):
-  __tablename__ = 'user'
-  id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-  discord_id = Column(BigInteger, nullable=False)
-  username = Column(String, nullable=False)
-  created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    __tablename__ = 'user'
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    discord_id = Column(BigInteger, nullable=False)
+    username = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
--- core/responses.py
+++ core/responses.py
@@ -1,23 +1,23 @@
 import random
 
 def handle_response(message):
-  p_message = message.lower()
+    p_message = message.lower()
 
-  if p_message == 'hello':
-    return "Hi there!"
+    if p_message == 'hello':
+        return "Hi there!"
 
-  if p_message.startswith('roll'):
-    max = 99
+    if p_message.startswith('roll'):
+        max = 99
 
     if len(message.split()) > 1:
-      custom_max = message.split()[1]
+        custom_max = message.split()[1]
 
-      if custom_max.isdigit():
-        max = int(custom_max)
-      else:
-        return "Please enter a valid number"
+        if custom_max.isdigit():
+            max = int(custom_max)
+        else:
+            return "Please enter a valid number"
 
-    return str(random.randint(1, max))
+        return str(random.randint(1, max))
 
-  if p_message == 'help':
-    return "`This is a help message`"
+    if p_message == 'help':
+        return "`This is a help message`"
