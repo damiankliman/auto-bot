daa4707573bb5f12f5aef35647b7e51148158873
damiankliman <klimandamian@gmail.com>
2023-01-03 19:57:07 -0800

    remove old message send handler

--- core/bot.py
+++ core/bot.py
@@ -5,16 +5,6 @@ from core.commands import handle_commands
 from core.database import get_db
 from core import services
 
-async def send_message(message, response: str, is_private: bool):
-    try:
-        if not response:
-            raise Exception("Command not found")
-
-        await message.author.send(response) if is_private else await message.channel.send(response)
-
-    except Exception as e:
-        print(e)
-
 def run_discord_bot():
     TOKEN = os.getenv('DISCORD_BOT_TOKEN')
     bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
