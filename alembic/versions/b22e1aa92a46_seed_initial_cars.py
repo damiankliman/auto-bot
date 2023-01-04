1660a0af89396134265c6a18ec3c384f2fca5020
damiankliman <klimandamian@gmail.com>
2023-01-04 00:43:33 -0800

    update car model

--- alembic/versions/b22e1aa92a46_seed_initial_cars.py
+++ alembic/versions/b22e1aa92a46_seed_initial_cars.py
@@ -1,24 +1,25 @@
 """Seed initial cars
 
-Revision ID: 3eb8b2f4634c
-Revises: b82fb7a3d204
-Create Date: 2023-01-03 21:14:49.039860
+Revision ID: b22e1aa92a46
+Revises: 38b0a9784f5e
+Create Date: 2023-01-04 00:36:48.079076
 
 """
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import table, column
 from sqlalchemy.dialects import postgresql
+from core.models import CarTypeType
 
 
 # revision identifiers, used by Alembic.
-revision = '3eb8b2f4634c'
-down_revision = 'b82fb7a3d204'
+revision = 'b22e1aa92a46'
+down_revision = '38b0a9784f5e'
 branch_labels = None
 depends_on = None
 
 
-def upgrade():
+def upgrade() -> None:
     car_table = table(
         "car",
         column("id", postgresql.UUID(as_uuid=True)),
@@ -26,10 +27,11 @@ def upgrade():
         column("make", sa.String),
         column("model", sa.String),
         column("trim", sa.String),
-        column("type", sa.String),
+        column("type", CarTypeType),
         column("horsepower", sa.Integer),
         column("weight", sa.Integer),
         column("price", sa.Integer),
+        column("order_code", sa.String),
     )
 
     op.bulk_insert(
@@ -41,10 +43,11 @@ def upgrade():
                 "make": "Nissan",
                 "model": "240sx",
                 "trim": "SE",
-                "type": "Hatchback",
+                "type": "HATCHBACK",
                 "horsepower": 134,
                 "weight": 2698,
                 "price": 3000,
+                "order_code": "S13A",
             },
             {
                 "id": "e159a9e5-de67-46a5-b58a-8475045bc946",
@@ -52,10 +55,11 @@ def upgrade():
                 "make": "BMW",
                 "model": "318",
                 "trim": "i",
-                "type": "Coupe",
+                "type": "COUPE",
                 "horsepower": 111,
                 "weight": 2348,
                 "price": 3000,
+                "order_code": "E36A",
             },
             {
                 "id": "680d3025-a530-45c4-9189-a503bac8c5f7",
@@ -63,10 +67,11 @@ def upgrade():
                 "make": "VW",
                 "model": "Golf",
                 "trim": "GTI",
-                "type": "Hatchback",
+                "type": "HATCHBACK",
                 "horsepower": 180,
                 "weight": 2674,
                 "price": 5000,
+                "order_code": "MK4A",
             },
             {
                 "id": "2bdb326b-d6db-42df-8761-3e28757431e3",
@@ -74,10 +79,11 @@ def upgrade():
                 "make": "Honda",
                 "model": "Civic",
                 "trim": "Type R",
-                "type": "Hatchback",
+                "type": "HATCHBACK",
                 "horsepower": 182,
                 "weight": 2359,
                 "price": 30000,
+                "order_code": "EK9A",
             },
             {
                 "id": "279042cd-e679-4dae-bd80-f0478ecbcc7f",
@@ -85,10 +91,11 @@ def upgrade():
                 "make": "Nissan",
                 "model": "Silvia",
                 "trim": "Spec R",
-                "type": "Coupe",
+                "type": "COUPE",
                 "horsepower": 247,
                 "weight": 2734,
                 "price": 50000,
+                "order_code": "S15A",
             },
         ],
     )
--- core/models.py
+++ core/models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import Column, String, BigInteger, Integer
+from sqlalchemy import Column, String, BigInteger, Integer, Enum
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql import text
 from core.database import Base
 import uuid
+import enum
 
 class User(Base):
     __tablename__ = 'user'
@@ -13,6 +14,21 @@ class User(Base):
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
     money = Column(BigInteger, nullable=True)
 
+class CarType(enum.Enum):
+    HATCHBACK = 'Hatchback'
+    COUPE = 'Coupe'
+    PICKUP = 'Pickup'
+    SEDAN = 'Sedan'
+    STATION_WAGON = 'Station Wagon'
+
+CarTypeType: Enum = Enum(
+    CarType,
+    name="car_type_type",
+    create_constraint=True,
+    metadata=Base.metadata,
+    validate_strings=True,
+)
+
 class Car(Base):
     __tablename__ = 'car'
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
@@ -20,7 +36,8 @@ class Car(Base):
     make = Column(String, nullable=False)
     model = Column(String, nullable=False)
     trim = Column(String, nullable=False)
-    type = Column(String, nullable=False)
+    type = Column(Enum(CarType), nullable=False)
     horsepower = Column(Integer, nullable=False)
     weight = Column(Integer, nullable=False)
     price = Column(Integer, nullable=False)
+    order_code = Column(String, unique=True, nullable=False)
