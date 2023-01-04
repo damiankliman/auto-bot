d35ea8f1c568769b82930b5d4fea26b1b8078f81
damiankliman <klimandamian@gmail.com>
2023-01-03 19:54:54 -0800

    refactor command invoke

--- core/bot.py
+++ core/bot.py
@@ -1,6 +1,7 @@
 import os
 import discord
-from core import responses
+from discord.ext import commands
+from core.commands import handle_commands
 from core.database import get_db
 from core import services
 
@@ -16,37 +17,16 @@ async def send_message(message, response: str, is_private: bool):
 
 def run_discord_bot():
     TOKEN = os.getenv('DISCORD_BOT_TOKEN')
-    client = discord.Client(intents=discord.Intents.all())
+    bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'{client.user} is connected and running')
+        print(f'{bot.user} is connected and running')
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+    @bot.before_invoke
+    async def before_invoke(ctx):
+        ctx.local_user = services.get_or_create_local_user(ctx.author)
 
-        if not message.content.startswith('/'):
-            return
+    handle_commands(bot)
 
-        username = str(message.author)
-        user_id = str(message.author.id)
-        message_content = str(message.content)[1:]
-        channel = str(message.channel)
-        local_user = services.get_or_create_local_user(message.author)
-        is_private = False
-
-        print(f"{username} with id:{user_id} in {channel}: {message_content}")
-        print(f"Local user: {local_user.username} with id: {local_user.id} with discord_id: {local_user.discord_id}")
-
-        if message_content.startswith('!'):
-            is_private = True
-            message_content = message_content[1:]
-
-        response = responses.handle_response(message_content, local_user)
-
-        await send_message(message, response, is_private)
-
-
-    client.run(TOKEN)
+    bot.run(TOKEN)
